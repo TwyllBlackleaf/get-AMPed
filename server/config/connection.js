a1239234cd9a09a3b6f77aaed759445db115b683
John Mohlenkamp <john@mohlenkamp.com>
2021-09-09 17:00:30 -0500

    added mongodb connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,20 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts', {
+//regular connection string
+// mongodb+srv://dbAdmin:buzzbuzz>@get-amped-cluster0.hvnlp.mongodb.net/get-AMPed-DB?retryWrites=true&w=majority
+
+/* full driver code connection
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://dbAdmin:buzzbuzz@get-amped-cluster0.hvnlp.mongodb.net/get-AMPed-DB?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+*/
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/get-amped', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
