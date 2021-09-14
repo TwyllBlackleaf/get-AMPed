20eebb0c69035c59cd712efb2f7f63dbe2422488
Twyll <twyllblackleaf@gmail.com>
2021-09-13 20:43:32 -0400

    set up components

---
+++ client/src/components/Main/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Login from "../Login";
+
+
+function Main() {
+    return (
+        <main>
+            Main
+            <Login></Login>
+        </main>
+    )
+}
+
+export default Main;
