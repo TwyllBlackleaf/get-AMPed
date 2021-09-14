44eb040f2c4cbf921a6103cee9abd5a96a485802
Twyll <twyllblackleaf@gmail.com>
2021-09-14 18:53:02 -0400

    login and signup forms

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -3,9 +3,36 @@ import "./Login.css";
 
 function Login() {
     return (
-        <div>
-            Login
-        </div>
+        <>
+            <form>
+                <h2>Log In</h2>
+                <div>
+                    <label for="loginName">Username: </label>
+                    <input type="text" name="loginName" id="loginName" required></input>
+                </div>
+                <div>
+                    <label for="loginPass">Password: </label>
+                    <input type="text" name="loginPass" id="loginPass" required></input>
+                </div>
+                <button type="submit">Log In</button>
+            </form>
+            <form>
+                <h2>Sign Up</h2>
+                <div>
+                    <label for="signupName">Username:</label>
+                    <input type="text" name="signupName" id="signupName" required></input>
+                </div>
+                <div>
+                    <label for="signupPass">Password:</label>
+                    <input type="text" name="signupPass" id="signupPass" required></input>
+                </div>
+                <div>
+                    <label for="signupEmail">Email:</label>
+                    <input type="text" name="signupEmail" id="signupEmail" required></input>
+                </div>
+                <button type="submit">Sign Up</button>
+            </form>
+        </>
     )
 }
 
