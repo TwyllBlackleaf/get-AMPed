5e814af6650d4a193decda4789b9bd9678760037
John Mohlenkamp <john@mohlenkamp.com>
2021-09-16 16:23:13 -0500

    updated npm packages for client/server - added rest structure

--- server/server.js
+++ server/server.js
@@ -1,29 +1,24 @@
 const express = require('express');
-// import ApolloServer
-const { ApolloServer } = require('apollo-server-express');
-// import our typeDefs and resolvers
-const { typeDefs, resolvers } = require('./schemas');
+const path = require('path');
 const db = require('./config/connection');
+const routes = require('./routes');
 
-const PORT = process.env.PORT || 3001;
 const app = express();
-// create a new Apollo server and pass in our schema data
-const server = new ApolloServer({
-  typeDefs,
-  resolvers
-});
+const PORT = process.env.PORT || 3001;
 
-// integrate our Apollo server with the Express application as middleware
-server.start().then(() => {
-  server.applyMiddleware({ app });
-});
-
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+
   });
 });
