059fd09506890820d8a9433e682445017c9de6fc
Twyll <twyllblackleaf@gmail.com>
2021-09-19 14:03:31 -0400

    pseudocoded functions and JSX needed for components

--- client/src/components/MyProfile/index.js
+++ client/src/components/MyProfile/index.js
@@ -1,9 +1,16 @@
 import React from "react";
+import PageContent from "../PageContent";
 
 function MyProfile() {
+    // function for checking token to make sure user is authenticated.
+        // if authenticated, const ownPage = true
+        // pass the ownPage constant to <PageContent> as prop so it can render edit buttons
+        // get user ID from token and pass it as prop to <PageContent>
+
     return (
         <main>
-            
+            {/* Check if ownPage === true. If not, <Redirect> to Login */}
+            <PageContent ownPage={ownPage} userId={userId}></PageContent>
         </main>
     )
 }
--- client/src/components/UserPage/index.js
+++ client/src/components/UserPage/index.js
@@ -1,9 +1,13 @@
 import React from "react";
+import PageContent from "../PageContent";
 
 function UserPage() {
+    // set constant ownPage to false, and pass it as prop to <PageContent> to prevent rendering of edit buttons
+    // get user ID from URL's :id parameter, and pass it as prop to <PageContent>
+
     return (
         <main>
-            User Page
+            <PageContent ownPage={ownPage} userId={userId}></PageContent>
         </main>
     )
 }
