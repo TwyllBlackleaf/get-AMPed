d8e9c34fe73539615288c186848821dbff55ecc0
Sue Kim Paulsen <suekimpaulsen@gmail.com>
2021-09-18 23:47:24 -0500

    singleuser username undefined issue fixed

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -15,8 +15,10 @@ const userController = {
   // get a single user by either their id or their username
   async getSingleUser({ user = null, params }, res) {
     const foundUser = await User.findOne({
-      $or: [{ _id: user ? user._id : params.id }, { username: params.username }],
-    });
+      $or: [{ _id: user ? user._id : params.id }, { username: user ? user.username : params.username }],
+    }).select('-__v');
+    console.log(foundUser)
+    console.log('=====', foundUser.username);
 
     if (!foundUser) {
       return res.status(400).json({ message: 'Cannot find a user with this id!' });
@@ -37,7 +39,7 @@ const userController = {
   // login a user, sign a token, and send it back (to client/src/components/LoginForm.js)
   // {body} is destructured req.body
   async login({ body }, res) {
-    const user = await User.findOne({ $or: [{ username: body.username }, { email: body.email }] });
+    const user = await User.findOne({ $or: [{ username: body.username }, { email: body.email }] }).select('-__v');
     if (!user) {
       return res.status(400).json({ message: "Can't find this user" });
     }
