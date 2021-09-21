33f7098c0a70c2cdeef7bab6fdf7ff819698194c
John Mohlenkamp <82176425+Mohlenkamp@users.noreply.github.com>
2021-09-21 09:19:59 -0500

    adding server side changes

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,5 +1,14 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes');
+const path = require('path');
+const apiRoutes = require('./api');
+
+router.use('/api', apiRoutes);
+
+// serve up react front-end in production
+router.use((req, res) => {
+  res.sendFile(path.join(__dirname, '../../client/build/index.html'));
+});
 
 router.use('/users', userRoutes);
 
