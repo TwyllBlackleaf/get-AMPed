f0849644595e75885ad3d6f7fb3e56a52e15dc22
Twyll <twyllblackleaf@gmail.com>
2021-09-21 16:31:32 -0400

    add state to PageContent and create component for edit form and functions for edit buttons

--- client/src/components/PageContent/index.js
+++ client/src/components/PageContent/index.js
@@ -1,11 +1,10 @@
-import React from "react";
+import { checkPropTypes } from "prop-types";
+import React, { useState } from "react";
+import EditForm from "./EditForm";
 
 function PageContent({ ownPage, userId }) {
-
-    // function for GETting User data by userId
-
-    // function for PUTting (or POSTing?) new/edited User data by userId
-    // double-check authentication before sending data
+    const [userData, setUserData] = useState({});
+    const [editMode, setEditMode] = useState("");
 
     // function for GETting all UserLinks associated with User by userId and storing them in an array
 
@@ -14,11 +13,54 @@ function PageContent({ ownPage, userId }) {
 
     // function for edit mode
 
+    function EditButton(props) {
+        return (
+            <button onClick={() => setEditMode(props.forItem)}>Edit</button>
+        )
+    }
+
+    function EditInput(props) {
+        if (editMode.forItem === props.forItem) {
+            return <EditForm forItem={props.forItem}></EditForm>
+        } else {
+            return <></>
+        }
+    }
+
+    function EditDiv(props) {
+        if (ownPage) {
+            return (
+                <div>
+                    <EditButton forItem={props.forItem}></EditButton>
+                    <EditInput forItem={props.forItem}></EditInput>
+                </div>
+                
+            )
+        } else {
+            return (
+                <></>
+            )
+        }
+    }
+
+    
+
+
+
     return (
         <>
             {/* Display user content: display name, photo, etc. */}
                 {/* Display edit buttons if ownPage === true */}
                 {/* Display copyable URL to link to page if ownPage === true */}
+            <div>
+                <h1 id="displayname">{userData.displayname}</h1>
+                <EditDiv forItem="displayname"></EditDiv>
+            </div>
+            <div>
+                <p id="aboutme">{userData.aboutme}</p>
+                <EditDiv forItem="aboutme"></EditDiv>
+            </div>
+            
             {/* Iterate through the array of UserLinks */}
                 {/* Display edit buttons if ownPage === true */}
             
--- client/src/components/UserPage/index.js
+++ client/src/components/UserPage/index.js
@@ -1,17 +1,15 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 import PageContent from "../PageContent";
 
-
-let ownPage;
-let userId;
-
 function UserPage() {
-    // set constant ownPage to false, and pass it as prop to <PageContent> to prevent rendering of edit buttons
     // get user ID from URL's :id parameter, and pass it as prop to <PageContent>
+    const { id } = useParams();
+
 
     return (
         <main>
-            <PageContent ownPage={ownPage} userId={userId}></PageContent>
+            <PageContent ownPage={false} userId={id}></PageContent>
         </main>
     )
 }
