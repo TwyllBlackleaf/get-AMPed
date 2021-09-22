3c70a949a283dfbbb3447e5363248129dbd2af20
Deb Orler <debbieorler@gmail.com>
2021-09-22 12:19:21 -0500

    Main Page Pt 1

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,12 +1,25 @@
 import React from "react";
-
+import "./style.css";
 
 function Home() {
-    return (
-        <section>
-            Home
-        </section>
-    )
+  return (
+    <div className="site-wrapper">
+      <div className="site-wrapper-inner">
+        <div className="container">
+          <div className="masthead clearfix">
+          </div>
+
+          <div className="inner cover">
+            <h1 className="cover-heading">Get AMPed</h1>
+            <p className="lead">
+            What is AMP? AMPed stands for About Me Page. <br></br> We are all about you getting your about me content<br></br> organized and set in on page. Get AMPED with us. 
+            </p>
+            <p className="lead">Get Started</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
