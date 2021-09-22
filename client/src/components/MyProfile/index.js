0ad9abc92a5a50802bfc35e0eb34a1b2166d3098
Twyll <twyllblackleaf@gmail.com>
2021-09-22 03:03:08 -0400

    get user data into MyProfile

--- client/src/components/MyProfile/index.js
+++ client/src/components/MyProfile/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PageContent from "../PageContent";
 import AuthService from "../../utils/auth";
 import { Redirect } from "react-router-dom";
@@ -7,8 +7,35 @@ import { getMe } from "../../utils/API";
 function MyProfile() {
     var ownPage = false;
 
+    const [userData, setUserData] = useState({});
 
-    const userData = getMe(AuthService.getToken());
+    const userDataLength = Object.keys(userData).length;
+
+    useEffect(() => {
+        const getUserData = async () => {
+            try {
+                const token = AuthService.getToken() ? AuthService.getToken() : null;
+                if (!token) {
+                    return false;
+                }
+                const response = await getMe(token);
+
+                if (!response.ok) {
+                    throw new Error("something went wrong!");
+                }
+
+                const user = await response.json();
+                setUserData(user);
+            } catch (err) {
+                console.log(err);
+            }
+        };
+
+        getUserData();
+    }, [userDataLength]);
+
+
+
 
     if (userData) {
         ownPage = true;
@@ -26,10 +53,24 @@ function MyProfile() {
         <main>
             {/* Check if ownPage === true. If not, <Redirect> to Login */}
             {userData.username}
-            <CheckRedirect></CheckRedirect>
+            {/* <CheckRedirect></CheckRedirect> */}
             <PageContent ownPage={ownPage} userData={userData}></PageContent>
         </main>
     )
+
+
+
+
+
+
+
+
+    return null;
+
+
+
+
+
 }
 
 export default MyProfile;
