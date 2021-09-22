1ed6c29294fd87ecf0491b5f28f050260e6d1a75
Deb Orler <debbieorler@gmail.com>
2021-09-22 10:58:35 -0500

    abandoning for bootstrap

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,38 +1,16 @@
 //Where we define our project color palette, type scale, fonts, breakpointes, border radius values
 
+const colors = require("tailwindcss/colors");
 
-const colors = require('tailwindcss/colors')
-
-module.export = {
-
-    theme: { 
-        screens: {
-        small:'480px',
-        md: '768px',
-        lg: '976px',
-        },
-
-    //set color palette for pages
-        colors: {
-            blue: colors.teal[500],
-            gray: colors.trueGray[300],
-            white: colors.blueGray[50],
-            black: colors.trueGray[900],
-        },
-    // 
-    fontFamily: {
-        sans: ['Ariel', 'Helvetica', 'sans-serif',],
-        serif: ['serif'],
-      },
-
-    extend: {
-        spacing: {
-        sm: '8px',
-        md: '16px',
-        lg: '24px',
-        xl: '48px',
-        },
-    }
-
-    }
-};  
+// tailwind.config.js
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
