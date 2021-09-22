75aa0a0bfd01c15394e8716b75df4b1714af73d4
Deb Orler <debbieorler@gmail.com>
2021-09-22 14:31:18 -0500

    header and main updated

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,23 +1,24 @@
 import React from "react";
-import "./style.css";
+import "../Home/style.css";
+
 
 function Home() {
   return (
-    <div className="site-wrapper">
-      <div className="site-wrapper-inner">
-        <div className="container">
-          <div className="masthead clearfix">
-          </div>
-
-          <div className="inner cover">
+    <div className="d-flex h-100 text-center text-white bg-dark">
+    <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+    <section className="px-3">
+    
             <h1 className="cover-heading">Get AMPed</h1>
             <p className="lead">
-            What is AMP? AMPed stands for About Me Page. <br></br> We are all about you getting your about me content<br></br> organized and set in on page. Get AMPED with us. 
+              What is AMP? AMPed stands for About Me Page. <br></br> We are all
+              about you getting your about me content<br></br> organized and set
+              in on page. Get AMPED with us.
             </p>
-            <p className="lead">Get Started</p>
-          </div>
-        </div>
-      </div>
+            <p className="lead">
+      <a href="#" class="btn btn-lg btn-secondary fw-bold border-white bg-white" value="Get Started">Get Started</a>
+    </p>
+    </section>
+    </div>
     </div>
   );
 }
